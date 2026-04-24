9e2e663038ac327dec59f71fde349e1327882cb0
Sébastien Le Callonnec <sebastien@weblogism.com>
2026-04-24 17:40:22 +0100

    fix:postgres shows as disconnected despite success inserting records.

--- Source/DBMS/PostgreSQL.cpp
+++ Source/DBMS/PostgreSQL.cpp
@@ -55,6 +55,10 @@ namespace IO
 				stats.reconnects++;
 			}
 		}
+		else
+		{
+			stats.connected = 1;
+		}
 
 		std::vector<QueuedEntry> batch;
 		{
@@ -438,7 +442,7 @@ namespace IO
 			run_thread = std::thread(&PostgreSQL::process, this);
 
 			Debug() << "DBMS: start thread, filter: " << Util::Convert::toString(filter.isOn());
-			
+
 			if (filter.isOn())
 				Debug() << ", Allowed: " << filter.getAllowed();
 
